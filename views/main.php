dd74705d2be560629a812ba2bfc944fe5de4c62b
dika <dikaemanuel@gmail.com>
2020-03-21 15:13:50 +0100

    Removed e

--- views/main.php
+++ views/main.php
@@ -84,9 +84,9 @@
                 <div class="col-md">
 				<?php
 
-					ini_set('display_errors', 1);
-					ini_set('display_startup_errors', 1);
-					error_reporting(E_ALL);
+					// ini_set('display_errors', 1);
+					// ini_set('display_startup_errors', 1);
+					// error_reporting(E_ALL);
 
 				?>
                     <?php Messages::display(); ?>
