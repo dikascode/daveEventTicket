1e8cc9cdc628d4acbef569c63053a2d1b8fc0aa5
dika <dikaemanuel@gmail.com>
2020-03-21 14:49:25 +0100

    e

--- classes/Controller.php
+++ classes/Controller.php
@@ -16,15 +16,15 @@
 
         protected function returnView($viewmodel, $fullview) {
             //name views folder same as class, file should also be named whatever the action is
-            
+
             // $view = 'views/'. get_class($this). '/' . $this->action. '.php'; // use this for localhost
 
             $view = get_class($this). '/' . $this->action. '.php'; //deployed state is including app/views cos it's mvc
 
             if($fullview) {
-                echo __DIR__ . " fullview";
+                echo __DIR__;
                 //load main layout file (html, head tags or things you want on every single page) that wraps around view
-                include('views/main.php');
+                include(__DIR__.'/main.php');
             } else {
                 include($view);
             }
